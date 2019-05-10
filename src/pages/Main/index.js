d1122678bd51837456085a2e2c19e7b5bf79a43d
Jordan <jordanboaz14@gmail.com>
2019-05-10 19:06:03 -0300

    Practicing styled components and animations

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,59 +1,105 @@
 import React from 'react';
+import { Animated } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import Header from '~/components/Header';
+import Tabs from '~/components/Tabs';
+import Menu from '~/components/Menu';
 
 import {
-  Text, Image, StyleSheet, Dimensions, ImageBackground, StatusBar,
-} from 'react-native';
+  Container,
+  CardHeader,
+  Card,
+  CardContent,
+  CardFooter,
+  Annotation,
+  Description,
+  Title,
+  Content,
+} from './styles';
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    flex: 1,
-    paddingHorizontal: 20,
-  },
-  fileName: {
-    fontWeight: 'bold',
-    marginTop: 5,
-  },
-  instructions: {
-    color: '#DDD',
-    fontSize: 14,
-    marginTop: 20,
-    textAlign: 'center',
-  },
-  logo: {
-    height: Dimensions.get('window').height * 0.11,
-    marginVertical: Dimensions.get('window').height * 0.11,
-    width: Dimensions.get('window').height * 0.11 * (1950 / 662),
-  },
-  welcome: {
-    color: '#fff',
-    fontSize: 22,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
+export default function Main() {
+  let offSet = 0;
+  const translateY = new Animated.Value(0);
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        },
+      },
+    ],
+    { useNativeDriver: true },
+  );
 
-const Main = () => (
-  <ImageBackground
-    source={{
-      uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/background.png',
-    }}
-    style={styles.container}
-    resizeMode="cover"
-  >
-    <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-    <Image
-      source={{
-        uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/rocketseat_logo.png',
-      }}
-      style={styles.logo}
-      resizeMode="contain"
-    />
-    <Text style={styles.welcome}>Bem-vindo ao Template Básico!</Text>
-    <Text style={styles.instructions}>Essa é a tela principal da sua aplicação =)</Text>
-    <Text style={styles.instructions}>Você pode editar a tela no arquivo:</Text>
-    <Text style={[styles.instructions, styles.fileName]}>src/pages/Main/index.js</Text>
-  </ImageBackground>
-);
+  function onHandlerStateChanged(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      const { translationY } = event.nativeEvent;
+      let opened = false;
+      offSet += translationY;
 
-export default Main;
+      if (translationY >= 100) {
+        opened = true;
+      } else {
+        translateY.setValue(offSet);
+        translateY.setOffset(0);
+        offSet = 0;
+      }
+
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => {
+        offSet = opened ? 380 : 0;
+        translateY.setOffset(offSet);
+        translateY.setValue(0);
+      });
+    }
+  }
+
+  return (
+    <Container>
+      <Header />
+
+      <Content>
+        <Menu translateY={translateY} />
+
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChanged}
+        >
+          <Card
+            style={{
+              transform: [
+                {
+                  translateY: translateY.interpolate({
+                    inputRange: [-350, 0, 380],
+                    outputRange: [-50, 0, 380],
+                    extrapolate: 'clamp',
+                  }),
+                },
+              ],
+            }}
+          >
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Seu saldo</Title>
+              <Description>R$ 658.695,54</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferência de R$ 20,00 recebida de Diego Lima hoje às 18:00
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
+      </Content>
+      <Tabs translateY={translateY} />
+    </Container>
+  );
+}
